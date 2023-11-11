bc8d49105b7688fadae49b37ec03b1a853484a55
Sardorbekt01 <sardorbekt175@gmail.com>
2023-11-11 20:52:43 +0300

    secon commit

--- post/models.py
+++ post/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Post(models.Model):
+    id = models.UUIDField(default=uuid.uuid4(),blank=False,null=False,primary_key=True)
+    name = models.CharField(max_length=20,blank=True,null=True)
+    title = models.CharField(max_length=100,null=True,blank=True)
+    date = models.DateField(auto_now=True)
+
--- post/views.py
+++ post/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def post(request):
+    content = {
+        'content':[{'id':'1','name':'cristiano','date':'today',},
+                   {'id':'2','name':'camavinga','date':'last day',},
+                   {'id':'3','name':'vini jr','date':'last week',},
+                   {'id':'4','name':'rafa','date':'last month',},
+                    ]
+    }
+    
+    return render(request=request,template_name='post.html',context=content)
 # Create your views here.
